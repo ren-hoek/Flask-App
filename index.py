7d17e54824e2b00187adb1a89434d1289dd7c94f
ren-hoek <ren-hoek@users.noreply.github.com>
2017-07-14 17:15:49 +0100

    changed index.py to include extra level of hierachy i.e. /app not /

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template 
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__,
+        static_url_path="/root_url/static")
 
-@app.route("/")
+@app.route("/root_url/")
 def home():
     return render_template('home.html')
 
